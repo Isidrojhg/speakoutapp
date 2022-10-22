3c029d935a0bf4b673689569be4ea2725c5fca9b
Isidroj <isidrohortelano@gmail.com>
2022-10-22 19:44:34 +0200

    add forget password form

---
+++ js/login.js
@@ -0,0 +1,19 @@
+class login {
+    constructor(form, fields) {
+        this.form = form;
+        this.fields = fields;
+        this.validateonSubmit();
+    }
+    validateonSubmit(){
+        let self = this;
+
+        
+
+    }
+}
+
+const from = document.querySelector(".loginForm");
+if(form) {
+    const fields = ["username", "password"];
+    const validator = new login(form, fields);
+}
